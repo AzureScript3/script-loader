60670716017b7fcbf34127ff1f562fb4e8ff43d9
AzureScript3 <126334041+AzureScript3@users.noreply.github.com>
2024-09-13 20:19:36 +0800

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,19 @@
+repeat
+    task.wait()
+until game.GameId ~= nil
+repeat
+    task.wait()
+until game:IsLoaded()
+
+if getgenv().loaded then
+    return
+end
+getgenv().loaded = true
+
+universeid = game.GameId
+
+setclipboard("https://discord.gg/RsMKm4ZWGd")
+
+if universeid == 8737899170 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AzureScript3/script-loader/main/main.lua",true))()
+end
